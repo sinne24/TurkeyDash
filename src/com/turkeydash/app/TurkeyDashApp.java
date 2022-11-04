0d13754edad571e75007018d08239d2fcb17695c
sinne24 <cindy.hernandez5224@gmail.com>
2022-11-04 16:06:31 -0600

    Added category enum, and aisle class, and began outlining methods in the app class

--- src/com/turkeydash/app/TurkeyDashApp.java
+++ src/com/turkeydash/app/TurkeyDashApp.java
@@ -1,7 +1,30 @@
 package com.turkeydash.app;
 
+import com.turkeydash.locationmodel.GeneralStore;
+import com.turkeydash.locationmodel.Home;
+import com.turkeydash.locationmodel.Location;
+import com.turkeydash.model.Dish;
+import com.turkeydash.model.Player;
+import com.turkeydash.storyboard.StoryBoard;
+
 public class TurkeyDashApp {
+
+    Location location;
+    GeneralStore generalStore;
+    Player player;
+    Home home;
+    Dish dish;
+    StoryBoard storyBoard;
 
     public void startScreen(){}
     public void enterName(){}
+
+    //TODO: figure out how to map ingredient enums, to dishes and player's basket
+
+    //TODO: Allow user to check basket
+
+    //TODO:Allow user to return to sections of general store repeatedly.
+
+
+
 }
---
+++ src/com/turkeydash/model/Category.java
@@ -0,0 +1,8 @@
+package com.turkeydash.model;
+
+/*
+ * 4 Categories?
+ */
+public enum Category {
+    DRY_GOODS, DAIRY, PRODUCE, ...
+}
