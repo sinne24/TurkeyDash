37d473a2f9a0989029dd3f98da1c0571ec634de2
sueanyv <valenciasue@hotmail.com>
2022-11-08 14:22:13 -0800

    edits to story, and home

---
+++ test/com/turkeydash/locationmodel/HomeTest.java
@@ -0,0 +1,12 @@
+package com.turkeydash.locationmodel;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class HomeTest extends TestCase {
+
+    @Test
+    public void testTallyPoints() {
+
+    }
+}
